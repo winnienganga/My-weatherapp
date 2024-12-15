7c96a73a4716152a89461a91dcf71de51f69c5ef
winnie <kwairimu96@gmail.com>
2024-12-16 00:54:15 +0300

    js search form

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hello");
+function startSearching(event){
+event.preventDefault();
+let searchBarInput =document.querySelector("#search-bar-input");
+let cityInput=document.querySelector("#city-element");
+cityInput.innerHTML = searchBarInput.value;
+}
+
+
+let searchBarElement = document.querySelector("#search-bar")
+searchBarElement.addEventListener("submit",startSearching);
